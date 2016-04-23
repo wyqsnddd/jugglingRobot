f4244a2306913823a22476d4c9617f1b1e7037cf
yuquan <wyqsnddd@gmail.com>
2016-04-23 14:53:52 +0200

    add figures

---
+++ robotPaddlingControl/common/include/robotPaddlingControl/robotPaddlingController.h
@@ -0,0 +1,14 @@
+#ifndef ROBOTPADDLINGCONTROLLER_H
+#define ROBOTPADDLINGCONTROLLER_H
+
+/**
+ * @file dumbo_Multi_obj_qp.h
+ * This is the header of the robot paddling controller class, which implements a constrained quadratic optimization method. 
+ * @author Yuquan 
+ */
+
+
+
+
+
+# endif
